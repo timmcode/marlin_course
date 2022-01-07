9bc804511603feecd1961bab523920bc3fbc3b7a
Dima Golubev <golubevda@ya.ru>
2022-01-07 03:23:30 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$base_url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$output = '';
+
+foreach (scandir(__DIR__) as $dir_item)
+    if(!in_array($dir_item, ['.','..','index.php','.idea']))
+        $output .= '<a href="' . $base_url . $dir_item . '">' . $dir_item . '</a>' . PHP_EOL;
+
+$output = '<pre>' . $output;
+$output .= '</pre>';
+
+echo $output;
